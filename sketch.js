cecd5644de29316fb89f8f5b51c4961d285fa870
Riz0tto <104946042+Riz0tto@users.noreply.github.com>
2023-10-10 00:16:06 +1300

    Added all current images for filter, started work on filter

--- sketch.js
+++ sketch.js
@@ -3,8 +3,8 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
+let sourceFile = "input_3.jpg";
+let maskFile   = "mask_3.png";
 let outputFile = "output_1.png";
 
 function preload() {
@@ -18,7 +18,7 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255, 0, 0);
+  background(0);
   sourceImg.loadPixels();
   maskImg.loadPixels();
 }
@@ -27,20 +27,81 @@ function draw () {
   for(let i=0;i<4000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
+
+    colorMode(RGB, 255);
     let pix = sourceImg.get(x, y);
+    let col = color(pix);
+    let h = hue(col);
+    let s = saturation(col);
+    let l = lightness(col);
+    colorMode(HSL, 360, 100, 100);
+    
+    const holdColour = color(h, s*1.5, l);
+    const wallColour = color(h, 0, l/2);
+
     let mask = maskImg.get(x, y);
-    fill(pix);
+
+    let gradientHue = map(y, 0, sourceImg.height, 0, 360);
+    let gradientColour = color(gradientHue, 100, 30, 0.1); 
+    
+
     if(mask[0] > 128) {
-      let pointSize = 10;
-      ellipse(x, y, pointSize, pointSize);
+
+      if(renderCounter>200){
+        push();
+        
+        translate(x, y);
+        strokeWeight(3);
+        stroke(holdColour);
+        point(0,0)
+
+        pop();
+      }
+
+
+      if(renderCounter<200 && renderCounter > 100){
+        push();
+
+        angleMode(DEGREES);
+        translate(x+10, y+10);
+        rotate(random(360));
+        strokeWeight(1);
+        stroke(gradientColour);
+        line(-10, 0, 10, 0);
+        line(0, -10, 0, 10);
+
+        pop();
+
+        push();
+
+        angleMode(DEGREES);
+        translate(x-10, y-10);
+        rotate(random(360));
+        strokeWeight(1);
+        stroke(gradientColour);
+        line(-10, 0, 10, 0);
+        line(0, -10, 0, 10);
+
+        pop();
+      }
+
     }
     else {
-      let pointSize = 20;
-      rect(x, y, pointSize, pointSize);    
+      if(renderCounter<100){
+        push();
+        angleMode(DEGREES);
+        translate(x, y);
+        rotate(random(360));
+        strokeWeight(1);
+        stroke(wallColour);
+        line(-5, 0, 5, 0);
+        line(0, -5, 0, 5);
+        pop();
+      }
     }
   }
   renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
+  if(renderCounter > 400) {
     console.log("Done!")
     noLoop();
     // uncomment this to save the result
